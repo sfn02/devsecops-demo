c981c88b2a4f7582eeac6212e5c44599a99cb05e
soufiane <fhaili.soufiane@gmail.com>
2025-05-23 22:09:26 +0100

    implemented refresh token blacklisting

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegistrationFormView, ProfileView, LoginView,LogoutView,RefreshToken
+from .views import RegistrationFormView, ProfileView, LoginView,LogoutView,RefreshTokenView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -10,9 +10,9 @@ urlpatterns = [
     path('auth/register/',RegistrationFormView.as_view(),name='register_view'),
     path('auth/login/',LoginView.as_view(),name='login_view'),
     path('auth/logout/',LogoutView.as_view(),name='logout'),
-    path('auth/token/refresh',TokenRefreshView.as_view(),name='refresh_token_view'),
+    path('auth/token/refresh/',RefreshTokenView.as_view(),name='refresh_token_view'),
     path('profile/',ProfileView.as_view(),name='profile_view'),
     path('profile/<int:pk>',ProfileView.as_view(),name='profile_view'),
-    path('refresh/',TokenRefreshView.as_view())
+    path('refresh/',RefreshTokenView.as_view())
     
 ]
