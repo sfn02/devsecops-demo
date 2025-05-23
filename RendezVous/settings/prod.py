697521c4e9ec8629bb4ad830cd493b773ecbcc1d
soufiane <fhaili.soufiane@gmail.com>
2025-05-23 22:09:26 +0100

    implemented refresh token blacklisting

--- RendezVous/settings/prod.py
+++ RendezVous/settings/prod.py
@@ -98,11 +98,11 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10), 
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15), 
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
-    'BLACKLIST_AFTER_ROTATION': False,
-    'UPDATE_LAST_LOGIN': False,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'UPDATE_LAST_LOGIN': True,
     'SIGNING_KEY': SECRET_KEY, 
     'ALGORITHM': 'HS256',
     'VERIFYING_KEY': None,
