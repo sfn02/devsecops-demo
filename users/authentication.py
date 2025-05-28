cfdd829a21f00b40faf64053a17688d42e282e6b
soufiane <fhaili.soufiane@gmail.com>
2025-05-28 15:33:20 +0100

    corrected xss bug

--- users/authentication.py
+++ users/authentication.py
@@ -17,26 +17,18 @@ class CustomJWTAuthentication(JWTAuthentication):
 
         raw_token = request.COOKIES.get('access_token')
         refresh_token_string = request.COOKIES.get('refresh_token')
-        print(f"trying with refresh token {refresh_token_string}")
         if raw_token:
             try:
-                print(11)
                 validated_token = self.get_validated_token(raw_token)
                 user = self.get_user(validated_token)
                 user.role = validated_token.get('role')
                 return user, validated_token
             except InvalidToken:
-                print("cookies authentication failed trying refresh !!")
                 if refresh_token_string:
-                    print("fall")
                     new_validated_token = RefreshToken(refresh_token_string) 
                     new_refresh = str(new_validated_token)
                     user = self.get_user(new_validated_token)
                     new_access = str(new_validated_token.access_token)
-                    print({
-                        "access":new_access,
-                        "refresh":new_refresh
-                    })
                     return user, new_validated_token
             except TokenError:
                 pass
